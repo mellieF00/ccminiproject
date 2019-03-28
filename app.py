13d0c992d3dc8f5a397e39c74a5aa4b4adfc9850
mellieF00 <miyofan@163.com>
2019-03-28 14:03:35 +0000

    app-3

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ session = cluster.connect()
 app = Flask(__name__)
 
 
-# Mainpage main page, welcome message and introduction.
+# Mainpage, welcome message and introductions.
 @app.route('/')
 def hello():
     name = request.args.get("name","World")
@@ -18,7 +18,12 @@ def hello():
         "<h3>MengyuanFan, studentID:180122581.</h3>" + \
         "<h3>MiniProject-Cloud Computer.</h3>")
 
-# First application, GET method
+'''
+First application, GET method
+Use id as the primary key, show the score and label of that id in my database
+Parameters: id
+return: your id, score and label or a message tells that your id does not exit
+'''
 @app.route('/get/<ide>') 
 def get_score(ide):
     rows = session.execute( """Select * From disease.stats where id = '{}'""".format(ide))
@@ -26,14 +31,25 @@ def get_score(ide):
         return('<h1>{} has ({}) score and the label is ({})!</h1>'.format(ide, row.score, row.label)),200
     return('<h1>That ID does not exist!</h1>'),404
 
-# Second-application, POST method
+
+'''
+Second-application, POST method,
+create a new row of data with id, score and label
+Parameters: id, score and label
+return: a message tells you that data is inserted succefully.
+'''
 @app.route('/post/<score>/<ide>/<label>')
 def post_rescore(score, ide, label):
     session.execute( """INSERT INTO disease.stats (score, id, label) VALUES ({},'{}','{}');""".format(score,ide,label))
     return('<h1>Your information is inserted successfully!</h1>'),200
 
 
-# Third-application, DELETE method
+'''
+Third-application, DELETE method
+delete a row of data with id
+Parameters: id
+return: a message tells you that the row of data is deleted succefully, or tells you your id doesn't exist.
+'''
 @app.route('/delete/<ide>')
 def delete_rescore(ide):
     rows = session.execute( """Select * From disease.stats where id = '{}'""".format(ide))
@@ -43,7 +59,11 @@ def delete_rescore(ide):
     return('<h1>Your information is deleted successfully!</h1>'),200
 
 
-# Fourth-application-1, New user Sign up: use hash to store password, store username
+'''
+Fourth-application, New user Sign up: use hash to store password, store username in my database
+Parameters: username, password
+return: a message tells you that you are signed up succefully, or tells you your username already exists.
+'''
 @app.route('/newuser/<user>/<password>')
 def newuser_password(user, password):
     rows = session.execute( """Select * From username.data where username = '{}'""".format(user))
@@ -54,8 +74,11 @@ def newuser_password(user, password):
         session.execute( """INSERT INTO username.data (username, password) VALUES ('{}','{}')""".format(user,pw_hash)),200
         return ('<h1>Your username and password are safely stored!</h1>')
 
-
-# Fourth-application-2, Old user login: check the user name and password
+'''
+Fourth-application, Old user login: check the username and password and login
+Parameters: username, password
+return: a message tells you that you are logged in succefully, or tells you your username doesn't exit or your password is not right.
+'''
 @app.route('/olduser/<user>/<password>')
 def olduser_password(user, password):
     rows = session.execute( """Select * From username.data where username = '{}'""".format(user))
